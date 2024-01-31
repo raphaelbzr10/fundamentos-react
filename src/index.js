5eff029531e36c4eb9887ce089123ccada436288
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2024-01-31 11:27:14 -0300

    primeiro componente #01

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import './index.css';
 
-const tag = <strong>Olá React!</strong>
+import Primeiro from './components/basicos/Primeiro';
 
 ReactDOM.render(
   <div>
-    {tag}
+    <Primeiro></Primeiro>
   </div>, 
   document.getElementById('root')
 );
