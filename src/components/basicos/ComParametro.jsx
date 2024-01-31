c53a8b9eba5f37264f06dc6e23731ba659ff298b
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2024-01-31 12:11:28 -0300

    componente com propriedade

---
+++ src/components/basicos/ComParametro.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ComParametro(props) {
+  const status = props.nota >= 7 ? 'aprovado' : 'reprovado';
+  return (
+    <div>
+      <h2>{props.titulo}</h2>
+      <p>
+        <strong>{props.aluno}</strong> tem nota <strong>{props.nota}</strong> e está {status}!
+      </p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import './index.css';
 
 import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
 
 ReactDOM.render(
   <div>
     <Primeiro></Primeiro>
+    <ComParametro 
+      titulo="Situação do Aluno" 
+      aluno="Pedro" nota={9.3}></ComParametro>
   </div>, 
   document.getElementById('root')
 );
